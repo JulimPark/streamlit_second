e4c7bebe10f4530aae1e8e7c928f3a1e2db1a72e
JulimPark <100160055+JulimPark@users.noreply.github.com>
2023-04-26 02:50:56 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,4 +4,34 @@ st.write("hello!~~bye!!!")
 df = pd.DataFrame(pd.read_csv('./exam_data.csv'))
 df1 = df['정답']
 
-df1.to_csv('./test.csv',mode='w',index=None)
+csv_file = 'results_option1.csv'
+if os.path.exists(csv_file):
+    # read from file
+    results_option1 = pd.read_csv(csv_file, index_col=False)
+else:
+    # create empty dataframe with the right columns & dtypes
+    results_option1 = pd.DataFrame(
+        {'time': np.array([]).astype('datetime64[ns]'),
+         'Primary Air Flow Rate': np.array([], dtype=np.float64),
+         'Primary Air Temperature': np.array([], dtype=np.float64),
+         'Reference Air Temperature': np.array([], dtype=np.float64),
+         }
+    )
+
+st.write('before')
+st.dataframe(results_option1)
+
+with st.form('input_form'):
+    qavalue = st.number_input('Primary Air Flow Rate')
+    travalue = st.number_input('Primary Air Temperature')
+    trvalue = st.number_input('Reference Air Temperature')
+    clickSubmit = st.form_submit_button('Submit')
+
+if clickSubmit:
+    timestamp = datetime.datetime.now()
+    results_option1.loc[len(results_option1)] = [timestamp, qavalue, travalue, trvalue]
+    results_option1.to_csv(csv_file, index=False)
+    st.write('after')
+    st.dataframe(results_option1)
+else:
+    st.markdown("Please submit to save")
