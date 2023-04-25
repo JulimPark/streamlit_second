0368a86d6ef81e939fbd02d71f3cbcf258da8f32
JulimPark <100160055+JulimPark@users.noreply.github.com>
2023-04-26 02:40:20 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,6 @@ import streamlit as st
 import pandas as pd
 st.write("hello!~~bye!!!")
 df = pd.DataFrame(pd.read_csv('./exam_data.csv'))
-df
+df1 = df['정답']
+
+df1.to_csv('./test.csv',mode='w',index=None)
