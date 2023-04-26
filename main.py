0a9762087f7e1132745ab2f1ea53d90454a3c75f
JulimPark <100160055+JulimPark@users.noreply.github.com>
2023-04-27 02:48:23 +0900

    Update main.py

--- main.py
+++ main.py
@@ -112,28 +112,28 @@ try:
             
 except:
     pass
-ddf = pd.DataFrame(
-    {'time': np.array([]).astype('datetime64[ns]'),
-     'Primary Air Flow Rate': np.array([], dtype=np.float64),
-     'Primary Air Temperature': np.array([], dtype=np.float64),
-     'Reference Air Temperature': np.array([], dtype=np.float64),
-     }
-)
+# ddf = pd.DataFrame(
+#     {'time': np.array([]).astype('datetime64[ns]'),
+#      'Primary Air Flow Rate': np.array([], dtype=np.float64),
+#      'Primary Air Temperature': np.array([], dtype=np.float64),
+#      'Reference Air Temperature': np.array([], dtype=np.float64),
+#      }
+# )
 
-st.write('before')
-st.dataframe(results_option1)
+# st.write('before')
+# st.dataframe(results_option1)
 
-with st.form('input_form'):
-    qavalue = st.number_input('Primary Air Flow Rate')
-    travalue = st.number_input('Primary Air Temperature')
-    trvalue = st.number_input('Reference Air Temperature')
-    clickSubmit = st.form_submit_button('Submit')
+# with st.form('input_form'):
+#     qavalue = st.number_input('Primary Air Flow Rate')
+#     travalue = st.number_input('Primary Air Temperature')
+#     trvalue = st.number_input('Reference Air Temperature')
+#     clickSubmit = st.form_submit_button('Submit')
 
-if clickSubmit:
-    timestamp = datetime.datetime.now()
-    results_option1.loc[len(results_option1)] = [timestamp, qavalue, travalue, trvalue]
-    results_option1.to_csv(csv_file, index=False)
-    st.write('after')
-    st.dataframe(results_option1)
-else:
-    st.markdown("Please submit to save")
+# if clickSubmit:
+#     timestamp = datetime.datetime.now()
+#     results_option1.loc[len(results_option1)] = [timestamp, qavalue, travalue, trvalue]
+#     results_option1.to_csv(csv_file, index=False)
+#     st.write('after')
+#     st.dataframe(results_option1)
+# else:
+#     st.markdown("Please submit to save")
