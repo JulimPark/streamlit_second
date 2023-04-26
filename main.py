2ef768b77eaba5736e2c47ecc01cd2374698240c
JulimPark <100160055+JulimPark@users.noreply.github.com>
2023-04-26 13:04:39 +0900

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ st.write('현재 응시자는 '+stu_name+'입니다')
 
 test_num = st.text_input('*:red[시험지코드]를 입력하세요 ', '0001')
 df1 = df[df['시험고유번호']==test_num].loc[:,]
-testname = df1.iat[0,4]
+testname = df1.iat[0,1]
 st.write(df1)
 st.write('문항 수:',len(df1.iat[0,5]))
 st.write('현재 시험지는 '+testname+'입니다')
