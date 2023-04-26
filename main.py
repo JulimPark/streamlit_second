872e2d3ec121ff537115824052e75f57baf757ae
JulimPark <100160055+JulimPark@users.noreply.github.com>
2023-04-26 14:14:28 +0900

    Update main.py

--- main.py
+++ main.py
@@ -26,22 +26,22 @@ if int(test_num) in list(df['시험고유번호']):
     testname = df1.iat[0,1]
     st.write(f'현재 시험지는 *:blue[{testname}] 입니다')
     start = st.button('시험시작')
-    if start:
-        timestamp = datetime.datetime.now()
-submit_answer = [num for num in range(len(test_answer))]
-for i in range(len(test_answer)):
-    if test_answer[i] in ['1','2','3','4','5']:
-        submit_answer[i] = st.radio(str(i+1)+'번 문항의 정답을 입력하세요.',('1','2','3','4','5'),key=None)
-    elif fnmatch.fnmatch(test_answer[i],'*,*'):
-        st.write(str(i+1)+'번 문항의 정답을 입력하세요.')
-        a1 = st.checkbox('1')
-        a2 = st.checkbox('2')
-        a3 = st.checkbox('3')
-        a4 = st.checkbox('4')
-        a5 = st.checkbox('5')
-        submit_answer[i] = [a1,a2,a3,a4,a5]
-    else:
-        submit_answer[i] = st.text_input(str(i+1)+'번 문항의 정답을 입력하세요.')
+if start:
+    timestamp = datetime.datetime.now()
+    submit_answer = [num for num in range(len(test_answer))]
+    for i in range(len(test_answer)):
+        if test_answer[i] in ['1','2','3','4','5']:
+            submit_answer[i] = st.radio(str(i+1)+'번 문항의 정답을 입력하세요.',('1','2','3','4','5'),key=None)
+        elif fnmatch.fnmatch(test_answer[i],'*,*'):
+            st.write(str(i+1)+'번 문항의 정답을 입력하세요.')
+            a1 = st.checkbox('1')
+            a2 = st.checkbox('2')
+            a3 = st.checkbox('3')
+            a4 = st.checkbox('4')
+            a5 = st.checkbox('5')
+            submit_answer[i] = [a1,a2,a3,a4,a5]
+        else:
+            submit_answer[i] = st.text_input(str(i+1)+'번 문항의 정답을 입력하세요.')
         
 
     
