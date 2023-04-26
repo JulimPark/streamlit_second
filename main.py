96af746f6bdcba2fe7df61f0f7bf3056e1182f11
JulimPark <100160055+JulimPark@users.noreply.github.com>
2023-04-26 15:02:13 +0900

    Update main.py

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ for i in range(len(test_answer)):
         submit_answer[i] = st.radio(str(i+1)+'번 문항의 정답을 입력하세요.',lst,index=0,horizontal=True)
     elif test_answer[i] in ['1,2','1,3','1,4','1,5','2,3','2,4','2,5','3,4','3,5','4,5']:
         st.write(str(i+1)+'번 문항의 정답을 입력하세요.')
-#         temp=[]
+        temp=[]
 #         a1 = st.checkbox('1')
 #         if a1==True:
 #             temp.append('1')
@@ -65,14 +65,19 @@ for i in range(len(test_answer)):
         checks = st.columns(5)
         with checks[0]:
             st.checkbox('1')
+            temp.append(checks[0])
         with checks[1]:
             st.checkbox('2')
+            temp.append(checks[1])
         with checks[2]:
             st.checkbox('3')
+            temp.append(checks[2])
         with checks[3]:
             st.checkbox('4')
+            temp.append(checks[3])
         with checks[4]:
             st.checkbox('5')
+            temp.append(checks[4])
 #         submit_answer[i] = ','.join(temp)
     else:
         submit_answer[i] = st.text_input(str(i+1)+'번 문항의 정답을 입력하세요.')
