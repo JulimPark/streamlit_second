e841acdea4fee59c406a18482d9fb263ccf9b682
JulimPark <100160055+JulimPark@users.noreply.github.com>
2023-04-27 22:00:31 +0900

    Update main.py

--- main.py
+++ main.py
@@ -131,14 +131,22 @@ try:
         st.dataframe(ddf)
         ddf.to_csv(csv_file, index=False)
         csv = convert_df(ddf)
-        st.download_button(
+       
+except:
+    pass
+try:
+    through_pass = st.text_input('*:blue[download password]를 입력하세요')
+    
+    if through_pass = '123123':
+         st.download_button(
             label="Download data as CSV",
             data=csv,
             file_name='take_exam_online.csv',
             mime='text/csv')
+     else:
+        pass
 except:
     pass
-
 #     st.write("???")
 # ddf = pd.DataFrame(
 #     {'time': np.array([]).astype('datetime64[ns]'),
