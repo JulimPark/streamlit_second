9d3caaf3d5d42ffca33d25a01d8b1e3343ea0958
JulimPark <100160055+JulimPark@users.noreply.github.com>
2023-04-27 15:48:17 +0900

    Update main.py

--- main.py
+++ main.py
@@ -107,7 +107,7 @@ if end_test:
     else:
         st.header(f':blue[시험고유번호 {test_num}]의 점수는 :red[{sum1}점] 입니다.')
         st.header(f'틀린 문항의 번호는 :green[{incorrect}]입니다.')
-        st.header(f'풀이 소요시간은 :purple[{remaintime}]입니다.')
+        st.header(f'풀이 소요시간은 :orange[{remaintime}]입니다.')
 
     csv_file = 'take_exam_online.csv'
     if os.path.exists(csv_file):
