aeb0e36ec8b57c91de58ac253b76acdb29088b0f
JulimPark <100160055+JulimPark@users.noreply.github.com>
2023-04-27 15:57:48 +0900

    Update main.py

--- main.py
+++ main.py
@@ -105,9 +105,9 @@ if end_test:
         else:
             incorrect.append(i+1)
     if sum(jumsu)==sum1:
-        st.header(f'참 잘 했습니다. :blue[시험고유번호 {test_num}]의 점수는 :red[{sum1}점] 입니다.')
+        st.header(f'참 잘 했습니다. 시험고유번호 :blue[{test_num}]의 점수는 :red[{sum1}점] 입니다.')
     else:
-        st.header(f':blue[시험고유번호 {test_num}]의 점수는 :red[{sum1}점] 입니다.')
+        st.header(f'시험고유번호 :blue[{test_num}]의 점수는 :red[{sum1}점] 입니다.')
         st.header(f'틀린 문항의 번호는 :green[{incorrect}]입니다.')
         st.header(f'풀이 소요시간은 :orange[{round(timestamp2-timestamp11,2)}]초 입니다.')
 
