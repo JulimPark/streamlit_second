23134846b56b616b344e722789f81af92051d8e3
JulimPark <100160055+JulimPark@users.noreply.github.com>
2023-05-23 03:42:13 +0900

    Update main.py

--- main.py
+++ main.py
@@ -139,7 +139,7 @@ try:
             ddf = pd.DataFrame(data_dict2)
         db2 = firestore.Client(credentials=creds, project="test-project-6e03a")
         # Create a reference to the Google post.
-        doc_ref2 = db2.collection("test").document('take_exam_data')
+        doc_ref2 = db2.collection("test").document(str(datetime.now()))
         doc_ref2.set({'학생이름':stu_name,'학생HP':0,'시험고유번호':test_num,'시험명':testname,'점수':sum1,'학생답':str(submit_answer),'맞은문항':str(correct),'틀린문항':str(incorrect),'문항별응시시간(초)':str(timelist),
                      '총응시시간(초)':sum(timelist),'응시일':take_day,'응시번호':0,'분류코드':str(df1.iat[0,12])})
         ddf.loc[len(ddf['학생이름'])] = [stu_name,0,test_num,testname,sum1,str(submit_answer),str(correct),str(incorrect),str(timelist),sum(timelist),take_day,1]
